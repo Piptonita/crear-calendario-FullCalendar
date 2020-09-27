97c65ab72df626a3cabc5560a2bd7531e93b6030
Guillermo Granados Gomez <guillermogranadosgomez@gmail.com>
2020-09-27 16:20:37 +0200

    3.modificar cabecera y pie

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -1,8 +1,26 @@
 var calendarDiv = document.getElementById('calendar');
 
 var calendar = new FullCalendar.Calendar(calendarDiv, {
-    locales: 'es', // añadimos esta línea en nuestro calendario
+    locales: 'es', 
     initialView: 'dayGridMonth', 
+    headerToolbar: { 
+        left: 'prev', 
+        center: 'title', 
+        right: 'next',
+
+    },
+    footerToolbar: {
+        left: 'prevYear',    
+        center: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth,today',
+        right: 'nextYear'
+    }, // añadimos la propiedad buttonText y traducimos los botones o ponemos lo que queramos:
+    buttonText: {
+        today: 'Hoy',
+        month: 'Mes',
+        week: 'Semana',
+        day: 'Día',
+        list: 'Listado'
+    }
 });
 
 calendar.render();
